45459574916d6d3990d0f37a718ec9ff9443bd5e
SUSHANT <SUSHANT@SUSHANT>
2018-10-07 00:16:05 +0530

    In controller

--- JactionApi/src/main/java/com/app/controller/LoginController.java
+++ JactionApi/src/main/java/com/app/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dto.UserDTO;
@@ -13,7 +14,7 @@ public class LoginController {
 	@Autowired
 	private LoginService service;
 
-	@RequestMapping(value = "/login")
+	@RequestMapping(value = "/login",method=RequestMethod.POST)
 	public String serviceProvider(UserDTO dto) {
 		String str = service.verifyLogin(dto);
 		return str;
