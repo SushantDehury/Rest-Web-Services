dbd6f5f12405882289b3ec80c676d3cbbb3d5262
SUSHANT <SUSHANT@SUSHANT>
2018-10-07 01:31:55 +0530

    add jsp and code

--- JactionApi/src/main/java/com/app/controller/LoginController.java
+++ JactionApi/src/main/java/com/app/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.app.controller;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +13,16 @@ import com.app.dto.UserDTO;
 import com.app.service.LoginService;
 
 @RestController
+@Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
 public class LoginController {
 
 	@Autowired
 	private LoginService service;
+	
+	@RequestMapping("/home")
+	public String getP() {
+		return "login";
+	}
 
 	@RequestMapping(value = "/login",method=RequestMethod.POST)
 	public String serviceProvider(UserDTO dto) {
