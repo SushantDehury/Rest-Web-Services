1ebecb42fe7e1bfc19115dff1361fb40eae64dc4
SUSHANT <SUSHANT@SUSHANT>
2018-10-07 00:03:32 +0530

    Implemented Controller calss

--- JactionApi/src/main/java/com/app/controller/LoginController.java
+++ JactionApi/src/main/java/com/app/controller/LoginController.java
@@ -1,8 +1,22 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.dto.UserDTO;
+import com.app.service.LoginService;
 
 @RestController
 public class LoginController {
+
+	@Autowired
+	private LoginService service;
+
+	@RequestMapping(value = "/login")
+	public String serviceProvider(UserDTO dto) {
+		String str = service.verifyLogin(dto);
+		return str;
+	}
 
 }
